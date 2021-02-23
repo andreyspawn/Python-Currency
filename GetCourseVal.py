4c13915daf6dd74adda43e167d578b9631a78aa2
andreyspawn <andrey.spawn@gmail.com>
2021-02-23 17:28:29 +0200

    Подготовка запроса на добавление курса валюты в таблицу Exchange_Rate

--- GetCourseVal.py
+++ GetCourseVal.py
@@ -4,6 +4,7 @@ import requests
 import configparser
 import os
 import mysql.connector
+from datetime import datetime
 
 #Считаем конфигурационный файл со статичным названием config.ini
 path_config = os.getcwd()+'\config.ini'
@@ -44,8 +45,23 @@ elif response.status_code != 200:
 # преобразуем ответ в объект json
 list_of_currency = response.json()
 
+# И теперь перебираем список валют по циклу
 print(list_of_currency)
-
+print("Этап номер ФИНАЛ")
 for curr in list_of_currency:
 	print(curr)
 	print(curr['CurrencyCode'])
+
+	# Делаем запрос из таблицы справочника валют по коду валюты
+	_SQL = f"SELECT CurrencyCode from currency where CurrencyCode = \'{curr['CurrencyCode']}\'"
+	cursor.execute(_SQL)
+	sql_result = cursor.fetchall()
+	# если валюта есть в справочнике просто добавляем ее в курс валют, в противном случае
+	# нужно отразить в журнале отсутствие данной замечательной валюты
+	if len(sql_result)!=0:
+		print(sql_result)
+		
+		_SQL = f"INSERT Exchange_rate(date_ExchangeRate,CurrencyCode,amount) VALUES({curr['StartDate']},{curr['CurrencyCode']},{curr['Amount']})"
+		print(_SQL)
+	# else:
+		# print("Да такая ФИГНЯ УЖЕ ЕСТЬ")
